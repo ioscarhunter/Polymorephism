77bfbd7bb3811d8281925ddc1c2e707c437dd757
ioscarhunter <ioscarhunter@gmail.com>
2014-02-20 18:56:44 +0700

    add in to vs 2013 project

--- Polymorphism/incomplete_code.py
+++ Polymorphism/incomplete_code.py
@@ -45,6 +45,8 @@ class Train( Transportation ):
 
 travel_cost = 0
 
+
+
 #trip = [ Walk("KMITL","KMITL SCB Bank",0.6),
          #Taxi("KMITL SCB Bank","Ladkrabang Station",5),
          #Train("Ladkrabang Station","Payathai Station",40,6),
